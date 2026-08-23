f434f37bcdf4e2431d6c1f7176786fa23563741c
sook-ar <jnanzarezban@gmail.com>
2026-08-23 23:09:23 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "souk-v1";
+const ASSETS = ["./", "./index.html", "./config.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
